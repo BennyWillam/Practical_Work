2b8632e7816069c9fedc50448f53abbc6f0efe09
Benjamin <benjamin.wilmen@my.jcu.edu.au>
2020-03-13 20:26:51 +1000

    Refactor password check program to use functions

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -1,10 +1,24 @@
-
 MINIMUM_PASSWORD_LENGTH = 4
 MAXIMUM_PASSWORD_LENGTH = 16
-password = input("Please enter a password between {} and {}:  ".format(MINIMUM_PASSWORD_LENGTH, MAXIMUM_PASSWORD_LENGTH))
 
-while not MINIMUM_PASSWORD_LENGTH <= len(password) <= MAXIMUM_PASSWORD_LENGTH:
-    print(" Sorry, your password {} is not the correct length".format("*" * len(password)))
-    password = input("Please enter a password: ")
 
-print("Your password is: {}".format("*" * len(password)))
+def main():
+    password = get_password()
+
+    print_result(password)
+
+
+def print_result(password):
+    print("Your password is: {}".format("*" * len(password)))
+
+
+def get_password():
+    password = input(
+        "Please enter a password between {} and {}:  ".format(MINIMUM_PASSWORD_LENGTH, MAXIMUM_PASSWORD_LENGTH))
+    while not MINIMUM_PASSWORD_LENGTH <= len(password) <= MAXIMUM_PASSWORD_LENGTH:
+        print(" Sorry, your password {} is not the correct length".format("*" * len(password)))
+        password = input("Please enter a password: ")
+    return password
+
+
+main()
